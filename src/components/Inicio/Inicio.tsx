90d166d5fdd2ea844813ad5cdfdad63eaad07f0a
Quintaneishon <ajitzi.quintana@gmail.com>
2021-04-17 00:09:29 -0500

    deploy 2

--- src/components/Inicio/Inicio.tsx
+++ src/components/Inicio/Inicio.tsx
@@ -6,7 +6,7 @@ export default function Inicio(){
         <section className="section-inicio moveInLeft">
             <div className="bg-video">
                 <video className="bg-video__content" autoPlay={true} loop={true} muted={true}>
-                    <source src="https://drive.google.com/uc?export=view&id=1Mrw08E-NW5wpvksnT9sZeTLs6cyoYehN" type='video/mp4'/>
+                    <source src="https://res.cloudinary.com/ddzl7xymq/video/upload/v1618635133/sm3rle78k6odipijxlui.mp4" type='video/mp4'/>
                     Your browser is not supported!
                 </video>
             </div>
